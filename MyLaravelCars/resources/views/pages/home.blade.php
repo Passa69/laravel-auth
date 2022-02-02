6c6002e1efc2667f1372446ffd5e53ddf8d6669e
tommaso <passarotto13@gmail.com>
2022-02-02 17:20:34 +0100

    aggiunta del codice per login/logout e register

--- MyLaravelCars/resources/views/pages/home.blade.php
+++ MyLaravelCars/resources/views/pages/home.blade.php
@@ -1,7 +1,47 @@
 @extends('mylayouts.main-layout')
 @section('content')
     
-    <h1>Hello World</h1>
+    @auth
+        <h1>{{ Auth::user() -> name }}</h1>
+        <a class="btn btn-secondary" href="{{ route('logout') }}">LOGOUT</a>
+    @else
+        <h1>If you wanne see my site you have to login/register</h1>
+    @endauth
 
-    <example-component></example-component>
+    <h2>Register</h2>
+    <form action="{{ route('register') }}" method="POST">
+
+        @method('POST')
+        @csrf
+
+        <label for="name">Name</label>
+        <input type="text" name="name"> <br>
+        <label for="email">E-mail</label>
+        <input type="text" name="email"> <br>
+        <label for="password">Password</label>
+        <input type="password" name="password"> <br>
+        <label for="password_confirmation">Password confirm</label>
+        <input type="password" name="password_confirmation"> <br>
+        <br>
+        <input type="submit" value="REGISTER">
+
+    </form>
+
+    <br><hr class="bg-white"><br>
+
+    <h2>Login</h2>
+        <form action="{{ route('login') }}" method="POST">
+
+        @method('POST')
+        @csrf
+
+        <label for="email">E-mail</label>
+        <input type="text" name="email"> <br>
+        <label for="password">Password</label>
+        <input type="password" name="password"> <br>
+        <br>
+        <input type="submit" value="LOGIN">
+
+    </form>
+
 @endsection
--- MyLaravelCars/routes/web.php
+++ MyLaravelCars/routes/web.php
@@ -12,3 +12,8 @@ use Illuminate\Support\Facades\Route;
 // Route::get('/home', 'HomeController@index')->name('home');
 
 Route::get('/', 'GuestController@home') -> name('home');
+
+Route::post('/register', 'Auth\RegisterController@register') -> name('register');
+
+Route::post('/login', 'Auth\LoginController@login') -> name('login');
+Route::get('/logout', 'Auth\LoginController@logout') -> name('logout');
